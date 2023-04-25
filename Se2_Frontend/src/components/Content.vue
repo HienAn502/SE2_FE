581f4d02f0ff8ec383df37d36e38991cf50e23a9
Condition for project GDSC <ducan.co.6789@gmai.com>
2023-04-25 13:04:41 +0700

    update homepage

--- Se2_Frontend/src/components/Content.vue
+++ Se2_Frontend/src/components/Content.vue
@@ -3,34 +3,7 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-import axios  from 'axios' 
-// import {useRouter} from 'vue-router'
-// const router = useRouter()
-// function handleClick(){
-//     router.push('/blog')
-// }
 
-// const infor = ref([]);
-// // Sử dụng fetch()
-// const getData = async () => {
-//     // ghep api
-//     const response = await fetch(
-//       "https://limitless-lake-55070.herokuapp.com/product/"
-//     );
-//     const data = await response.json(); // lam cho data o dang object
-//     infor.value = data.slice(0, 8);
-// };
-// getData();
-
-// sử dụng axios
-// const infor = ref([])
-// const getData = async () => {
-//    const repsonse = await axios.get('https://limitless-lake-55070.herokuapp.com/product/')
-//    const data = await repsonse.data
-//    infor.value = data.slice(0,8)
-// }
-// getData()
 </script>
 
 <style scoped> 
