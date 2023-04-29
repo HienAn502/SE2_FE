045a3dc043e2b5e79e87c7dae116a59af06393d2
DucNguyen <2001040040@s.hanu.edu.vn>
2023-04-30 04:46:38 +0700

    update

--- Se2_Frontend/src/router/index.js
+++ Se2_Frontend/src/router/index.js
@@ -10,6 +10,15 @@ import Earings from "../views/Earings.vue"
 import About from "../views/About.vue"
 import SignUp from "../components/SignUp.vue"
 import Login from "../components/Login.vue"
+import AddCategory from "../views/Category/AddCategory.vue"
+import EditCategory from "../views/Category/EditCategory.vue"
+import Category from "../views/Category/Category.vue"
+import Admin from "../views/Admin.vue"
+import Product from "../views/Product/Product.vue"
+import EditProduct from "../views/Product/EditProduct.vue"
+import AddProduct from "../views/Product/AddProduct.vue"
+import Voucher from "../views/Voucher/Voucher.vue"
+import AddVoucher from "../views/Voucher/AddVoucher.vue"
 
 const routes = [{
         path: '/',
@@ -47,7 +56,45 @@ const routes = [{
     {
         path: '/login',
         component: Login
+    },
+    { 
+        path: '/admin/category/add',
+        component: AddCategory
+    },
+    {
+        path: '/admin/category/edit',
+        component: EditCategory
+    },
+    {
+        path: '/admin/category',
+        component: Category
+    },
+        //admin home page
+    {
+        path:'/admin',
+        component: Admin
+    },
+    {
+        path:'/admin/product',
+        component: Product
+    },
+    {
+        path: '/admin/product/add',
+        component: AddProduct
+    },
+    {
+        path: '/admin/product/edit',
+        component: EditProduct
+    },
+    {
+        path:'/admin/voucher',
+        component: Voucher
+    },
+    {
+        path: '/admin/voucher/add',
+        component: AddVoucher
     }
+
 ]
 const router = createRouter({
     history: createWebHistory(),
