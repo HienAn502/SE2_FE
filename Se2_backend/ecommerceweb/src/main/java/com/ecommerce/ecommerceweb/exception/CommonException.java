d5e2f193533f50406c64cb927e171a704ac3ba9f
HienAn502 <92548686+HienAn502@users.noreply.github.com>
2023-05-07 19:54:25 +0700

    remove redundant files

--- Se2_backend/ecommerceweb/src/main/java/com/ecommerce/ecommerceweb/exception/CommonException.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.ecommerceweb.exception;
-
-public class CommonException extends IllegalArgumentException {
-    public CommonException(String msg){
-        super(msg);
-    }
-}
--- Se2_backend/ecommerceweb/src/main/java/com/ecommerce/ecommerceweb/exception/FailAuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.ecommerceweb.exception;
-
-public class FailAuthenticationException extends IllegalArgumentException {
-    public FailAuthenticationException(String msg){
-        super(msg);
-    }
-}
